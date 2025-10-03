2987af8eb5d500203980d4d911f005de609f650f
Heewon <139496729+heeone1@users.noreply.github.com>
2025-10-03 18:42:59 +0900

    [level 1] Title: 신고 결과 받기, Time: 160.11 ms, Memory: 45.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -0,0 +1,29 @@
+#한 번에 한 명의 유저를 신고
+#한 유저를 여러 번 신고할 수도 있지만, 동일한 유저에 대한 신고 횟수는 1회로 처리
+#게시판 이용 정지를 시키면서 정지 메일을 발송 / k번 이상 신고된 유저는 게시판 이용이 정지
+#해당 유저를 신고한 모든 유저에게 정지 사실을 메일로 발송 / 
+
+
+def solution(id_list, report, k):
+    # 중복 제거
+    report = set(report)
+
+    # 신고 기록 분리(초기화)
+    reports = {user: [] for user in id_list}  # 각 유저가 누구를 신고했는지
+    counts = {user: 0 for user in id_list}    # 각 유저가 신고당한 횟수
+
+    for r in report:
+        a, b = r.split()     # "신고자 피신고자"
+        reports[a].append(b)
+        counts[b] += 1
+
+    # 정지된 사람들
+    stopped = {user for user, c in counts.items() if c >= k}
+    
+    # 각 유저별 메일 개수 계산
+    answer = []
+    for user in id_list:
+        cnt = sum(1 for target in reports[user] if target in stopped)
+        answer.append(cnt)
+
+    return answer
